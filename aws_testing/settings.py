01454c49d9875ec997f001ddd9f4b4e01e94cf75
kothapallyshivasai <shivasaikothapally@gmail.com>
2024-05-02 06:15:23 +0530

    Debug

--- aws_testing/settings.py
+++ aws_testing/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7z^ypvulalhgvx#-q*i2f&cno8ti)ij24@^i7jxqtp062h-_nd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
